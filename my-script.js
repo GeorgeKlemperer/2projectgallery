a95e69ff55134316bebbf84ca0c8ad9147f56693
GeorgeKlemperer <george@klemperer.co.uk>
2023-03-12 22:48:28 +0000

    popups working

---
+++ my-script.js
@@ -0,0 +1,11 @@
+  // When the user clicks on div, open the popup
+  function popupFunction1() {
+    var popup = document.getElementById("myPopup1");
+    popup.classList.toggle("reveal");
+  }
+
+
+  function popupFunction3() {
+    var popup = document.getElementById("myPopup3");
+    popup.classList.toggle("reveal");
+  }
